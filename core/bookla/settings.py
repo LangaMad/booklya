f5d01e41a2c0f7554c89ee95b374c25e06565412
sAbA-pixel-3 <seconddonutbs@gmail.com>
2025-02-08 19:13:39 +0600

    secTEST

--- core/bookla/settings.py
+++ core/bookla/settings.py
@@ -14,6 +14,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 # bla bla
 # qwerty
 
+# myself
+# men
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
