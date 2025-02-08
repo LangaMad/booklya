89ee381432b0e6f58d8095a923a03dd68910d8b9
LangaMad <artykovh@gmail.com>
2025-02-08 18:58:06 +0600

    test

--- core/bookla/settings.py
+++ core/bookla/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+# Shabdan ,Nurik,Atabek
+
 
 from pathlib import Path
 
