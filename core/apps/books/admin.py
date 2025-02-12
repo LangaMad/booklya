4c4263d5b1e6a6905a65191b0bff607f89d1551c
sAbA-pixel-3 <seconddonutbs@gmail.com>
2025-02-12 11:04:44 +0600

    models: Book, Genre, Tag, Language and admin

--- core/apps/books/admin.py
+++ core/apps/books/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display =['name']
+    prepopulated_fields = {'slug':('name',)}
+
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'author',
+        'isbn',
+        'rate',
+        'edition',
+        'publisher',
+        'description',
+        'image',
+        'pages',
+        'book_format',
+        'is_active',
+        'display_genres',
+        'display_tags',
+    ]
+    list_filter = ['is_active',
+                   'genres',
+                   'tags']
+    search_fields = ['title',
+                     'author',
+                     'isbn']
+    filter_horizontal = ('tags', 'genres')
+
+    def display_genres(self, obj):
+        return ", ".join([genre.name for genre in obj.genres.all()])
+    display_genres.short_description = "Жанры"
+
+    def display_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    display_tags.short_description = "Теги"
+
+
+
+@admin.register(BookLanguage)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = [
+        'book',
+        'language',
+        'book_file'
+    ]
+    list_filter = ['language']
+    search_fields = ['book__title', 'language']
--- core/bookla/settings.py
+++ core/bookla/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-24qwid8t(v1@3r7i*lewj4byf2$t74s%k(&t@&a&*92+kxhx1%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'), # or 'localhost'
+        'PORT': config('DB_PORT', default=5432), # 5432
     }
 }
 
@@ -103,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
@@ -116,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
