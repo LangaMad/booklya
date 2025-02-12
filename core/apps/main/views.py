8862cd0b7f592c4bd27f7252bc4a6b88763e9bd6
jnrslm3 <jumabekovn43@gmail.com>
2025-02-12 19:13:51 +0600

    added ContactUs model in database, added ContacUsForm and contact_us in admin , added form and crsf token and form in contact_us.html

--- core/apps/main/views.py
+++ core/apps/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
+from .forms import ContactUsForm
 
 class HomeView(TemplateView):
     template_name = 'pages/index.html'
-
-class ContactUs(TemplateView):
+#
+class ContactUs(CreateView):
     template_name = 'pages/contact_us.html'
+    form_class = ContactUsForm
+    success_url = '/'
