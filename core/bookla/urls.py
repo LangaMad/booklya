afc8a340c849ba497472bd02593fb8c5629ad7fa
jnrslm3 <jumabekovn43@gmail.com>
2025-02-15 18:23:43 +0600

    c

--- core/bookla/urls.py
+++ core/bookla/urls.py
@@ -20,4 +20,12 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
+    path('blog/' , include('apps.blog.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
