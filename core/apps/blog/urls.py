4bc9877d60a1eabc5cfeecec89046dc864d5a234
LangaMad <artykovh@gmail.com>
2025-02-18 19:25:11 +0600

    update some models, add models & view % urls & forms for cart(app)

--- core/apps/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from tkinter.font import names
-
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', BlogView.as_view(), name = 'blog'),
-    path('detail/', DetailBlogView.as_view(), name = 'detail_blog'),
-]
--- core/bookla/settings.py
+++ core/bookla/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'apps.books',
     'apps.cart',
     'apps.main',
-    'apps.blog',
+    # 'apps.blog',
 
 ]
 
