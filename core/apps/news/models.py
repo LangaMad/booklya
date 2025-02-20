b2e01dea543732f7f89ba901fc87b34614f66200
Atabek77-alt <atabekis2006@gmail.com>
2025-02-20 23:40:25 +0600

    news

--- core/apps/news/models.py
+++ core/apps/news/models.py
@@ -12,10 +12,12 @@ class BookNews(models.Model):
     description = models.TextField('Описание')
     image = models.ImageField('Фото книги', upload_to='books/')
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    created_at1 = models.DateField('Время',blank=True, null=True )
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2, null=True, blank=True)
     tags = models.ManyToManyField(Tag, related_name='news_tag' )
     category = models.ManyToManyField(PostCategory, related_name='news_category')
-    news_detail = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_news')
+    location = models.CharField('Место', max_length=80, null=True, blank=True)
+
 
     def __str__(self):
         return self.title
--- core/apps/news/views.py
+++ core/apps/news/views.py
@@ -11,16 +11,16 @@ class NewsView(ListView):
     queryset = BookNews.objects.all().order_by()
     paginate_by = 4
 
-    def post_list(request):
-        # Получаем все посты
-        posts = BookNews.objects.all()
-
-        # Настроим пагинацию
-        paginator = Paginator(posts, 5)  # Показывать по 5 постов на странице
-        page_number = request.GET.get('page')  # Получаем номер текущей страницы
-        page_obj = paginator.get_page(page_number)
-
-        return render(request, 'your_template.html', {'page_obj': page_obj})
+    # def post_list(request):
+    #     # Получаем все посты
+    #     posts = BookNews.objects.all()
+    #
+    #     # Настроим пагинацию
+    #     paginator = Paginator(posts, 5)  # Показывать по 5 постов на странице
+    #     page_number = request.GET.get('page')  # Получаем номер текущей страницы
+    #     page_obj = paginator.get_page(page_number)
+    #
+    #     return render(request, 'your_template.html', {'page_obj': page_obj})
 
 class DetailNewsView(DetailView):
     template_name = 'pages/news_detail.html'
