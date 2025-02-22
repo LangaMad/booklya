0594aa5ea4c540e3f811049bfa9f811375c7b0fb
Atabek77-alt <atabekis2006@gmail.com>
2025-02-22 18:52:43 +0600

    main

--- core/apps/news/views.py
+++ core/apps/news/views.py
@@ -8,19 +8,10 @@ class NewsView(ListView):
     model = BookNews
     template_name = 'pages/news_list.html'
     context_object_name = 'news'
-    queryset = BookNews.objects.all().order_by()
-    paginate_by = 4
+    queryset = BookNews.objects.all().order_by('-created_at')
+    paginate_by = 3
 
-    # def post_list(request):
-    #     # Получаем все посты
-    #     posts = BookNews.objects.all()
-    #
-    #     # Настроим пагинацию
-    #     paginator = Paginator(posts, 5)  # Показывать по 5 постов на странице
-    #     page_number = request.GET.get('page')  # Получаем номер текущей страницы
-    #     page_obj = paginator.get_page(page_number)
-    #
-    #     return render(request, 'your_template.html', {'page_obj': page_obj})
+
 
 class DetailNewsView(DetailView):
     template_name = 'pages/news_detail.html'
